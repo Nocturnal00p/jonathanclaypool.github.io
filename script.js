9247695e6b50e56b57249bf92ad5a865d5ee253c
Nocturnal00p <jclaypool55@gmail.com>
2016-05-30 15:01:45 -0500

    Changing out sites

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("foot01").innerHTML =
+"<p>&hearts;  " + new Date().getFullYear() + " Jonathan Claypool </p>";
+ 
+document.getElementById("nav01").innerHTML = 
+"<ul id='menu'>" + "<li><a href='index.html'>Home</a></li>" +
+"<li><a href='casualties.html'>Casualties</a></li>" + "<li><a href='Wall.html'>Wall of Heroes</a></li>"
++ "</ul>"
