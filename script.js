106b2fd92de7cbab10219b423358f55612a9a9b0
Nocturnal00p <jclaypool55@gmail.com>
2016-05-30 18:00:16 -0500

    awjsajda

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var i=0;
+var names = [];
+
 document.getElementById("foot01").innerHTML =
 "<p>&hearts;  " + new Date().getFullYear() + " Jonathan Claypool </p>";
  
@@ -6,9 +9,11 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='Casualties.html'>Casualties</a></li>" + "<li><a href='Wall.html'>Wall</a></li>"
 + "</ul>";
 
+document.getElementById("woh").innerHTML = full;
 
 function displayName() {
 var fname =	document.getElementById("firstname").text; 
 var lname =	document.getElementById("lastname").text;
-document.getElementById("woh").innerHTML += fname + " " + lname + "<br>";
+var full = fname + " " + lname + "<br>";
+names.push(full);
 }
