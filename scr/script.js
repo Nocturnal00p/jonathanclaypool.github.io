993dd0dff3930c124dfab4cc82653ae9d35c1395
Nocturnal00p <jclaypool55@gmail.com>
2016-12-24 00:16:35 -0600

    adada
    ddad

--- scr/script.js
+++ scr/script.js
@@ -1,10 +1,12 @@
 var fpPics = [document.getElementById("frontpage1"), document.getElementById("frontpage2")];
 
 var w = window.innerWidth;
+
 if (w < 1000)
 	{
 	document.getElementById("menu").style.borderRight = "5px solid #706f6f";
 	document.getElementById("menu").style.borderBottom = "5px none #706f6f";
+	document.getElementById("h1").style.fontSize = "30px";
 	}
 console.log(w);
 
