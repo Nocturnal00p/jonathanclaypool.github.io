725d2b40a15a48e3a3baa0c6f60873cb09d197de
Nocturnal00p <jclaypool55@gmail.com>
2016-12-23 23:07:27 -0600

    sdasdad
    dada

--- scr/script.js
+++ scr/script.js
@@ -1,5 +1,7 @@
-var fpPics = [document.getElementById('frontpage1'), document.getElementById('frontpage2')]
+var fpPics = [document.getElementById("frontpage1"), document.getElementById("frontpage2")];
+
 
 function myFunction(){
 	fpPics[0].src = fpPics[1].src;
+	document.getElementById("menu").style.borderRight = "5px solid #706f6f";
 }
