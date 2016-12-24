7feea08dd4ecc80b3815247570e113456f5cc492
Nocturnal00p <jclaypool55@gmail.com>
2016-12-23 23:45:21 -0600

    dsdsd
    sdsdsd

--- scr/script.js
+++ scr/script.js
@@ -1,7 +1,13 @@
 var fpPics = [document.getElementById("frontpage1"), document.getElementById("frontpage2")];
 
+var w = window.innerWidth;
+if (w < 1000)
+	{
+	document.getElementById("menu").style.borderRight = "5px solid #706f6f";
+	document.getElementById("menu").style.borderBottom = "5px none #706f6f";
+	}
+console.log(w);
 
 function myFunction(){
 	fpPics[0].src = fpPics[1].src;
-	document.getElementById("menu").style.borderRight = "5px solid #706f6f";
 }
